f08c3e3382c5372be31120c54c97dd4e3b0854f4
xkz9 <leonardomilanov@gmail.com>
2023-11-04 12:20:21 +0000

    04/11/23: We learnt about The different data types in Python and the varialble naming conventions

---
+++ Basic.py
@@ -0,0 +1,27 @@
+# print("Hello")
+# name = input("Enter Your Name ")
+# print (name)
+# data types in python
+# int = 34
+# float = 0.19
+# bool = True False
+# string "Hello" 'Bye' """Hello""" '''Hello'''
+# Char "a"
+
+# Variable Naming Convention
+
+# 1. We can have alphabets anywhere in the variable
+# name = "Hello"
+# print (name)
+
+# 2. You can put numbers anywhere in a variable except for the start.
+# 9n1ame3 = "Hello2"
+# print (9n1ame3)
+
+# 3. You cant put symbols anywhere except '_'
+# _na_me_ = "Work"
+# print (_na_me_) 
+
+# 4. You cannot use spaces in a variable name
+# na me = "Work"
+# print = (na me) 
