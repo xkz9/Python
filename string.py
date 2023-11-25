7b5511a41c158cd093ce94a2db3f4e0f8ace9b9e
xkz9 <leonardomilanov@gmail.com>
2023-11-25 10:01:38 +0000

    Learnt to string slice,

--- string.py
+++ string.py
@@ -10,9 +10,10 @@
 # print(mystr[30])  
 # print(mystr[3])     
 # print(mystr[-35])  
-mystr = "Leonardo and Ostap are learning python"
+# mystr = "Leonardo and Ostap are learning python"
 
-# list slicing
+# List slicing
+
 # start is included but end is not included.  
 # print(mystr[0:8])  
 # print(mystr[-39:-30])
@@ -22,3 +23,23 @@ mystr = "Leonardo and Ostap are learning python"
 # print(mystr[-6:])
 # print(mystr[32:])
 # print(mystr[-6:39])
+original_string = "Python is a powerful programming language "
+substring_2 = original_string[3:10:2]
+print(substring_2)
+# reversed_string = original_string[-1:-41:-1]
+# print(reversed_string[5:13:1])
+# print(original_string[-1:-41:-1])
+# print(original_string[-9:-1:1])
+# print(original_string[-9:41:1])
+# print(original_string[33:-1:1])
+# print(original_string[33:41:1])
+# print(original_string[6:15:2])
+# print(original_string[6])
+# print(original_string[8])
+# print(original_string[10])
+# print(original_string[12])
+# print(original_string[14])
+# print(original_string[0:10:3])
+# mystr = "Python is very easy"
+# print(mystr[0:5:2])
+
