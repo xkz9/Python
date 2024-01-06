32b8f69d0ccfa852cf04820d2918385221b36145
xkz9 <leonardomilanov@gmail.com>
2024-01-06 10:01:15 +0000

    How to use python turtle and make a square riangle and circle

---
+++ python_turtle.py
@@ -0,0 +1,68 @@
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.pencolor("black") 
+my_turtle.fillcolor("black")
+# my_turtle.forward(100)
+my_turtle.speed(1)
+
+# backward square
+
+# my_turtle.begin_fill()
+# my_turtle.backward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.left(90)
+# my_turtle.forward(100)
+# my_turtle.left(90)
+# my_turtle.forward(100)
+# my_turtle.end_fill() 
+
+# forward square 
+
+# my_turtle.begin_fill()
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.end_fill()
+
+# short code square
+
+# for i in range(4):
+#     my_turtle.forward(100)
+#     my_turtle.right(90)
+
+# short code rectangle
+
+# my_turtle.begin_fill()
+# for i in range(2): 
+#     my_turtle.forward(100)
+#     my_turtle.right(90)
+#     my_turtle.forward(55)
+#     my_turtle.right(90)
+# my_turtle.end_fill()
+
+# forward triangle
+
+# my_turtle.forward(100)
+# my_turtle.right(120)
+# my_turtle.forward(100)
+# my_turtle.right(120)
+# my_turtle.forward(100)
+# my_turtle.right(120)
+
+# short code triangle
+
+# my_turtle.begin_fill
+# for i in  range(3):
+#     my_turtle.forward(100)
+#     my_turtle.right(120)
+
+# short code circle
+
+# for i in range(360):
+#     my_turtle.forward(1)
+#     my_turtle.right(1)
