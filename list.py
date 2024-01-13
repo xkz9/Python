2f515faf454adc55ca6f8d91902eebe58cc5e1ff
xkz9 <leonardomilanov@gmail.com>
2024-01-13 10:02:44 +0000

    List Basics

---
+++ list.py
@@ -0,0 +1,48 @@
+mylist = ["Leonardo's list", 9, 79, False]
+
+# The apppend funtion adds whatever value you want to the end of your list.
+
+# print (mylist)
+# mylist.append("Hello")
+# print(mylist)
+
+# The remove function removes whatever is present in the brackets mylist.remove(xx)
+
+# mylist.remove(False)
+# print(mylist)
+
+# The pop function is used to remove a particular value by using it's index. 
+
+# mylist.pop(2)
+# print(mylist)
+
+# Th einsert function adds a particular value at  a particular index in the list
+
+# mylist.insert(2, 200)
+# print(mylist)
+
+# mylist.reverse()
+# print(mylist)
+
+# The clear function gives you an empty list when ran (removes all elements of the list). 
+
+# mylist.clear()
+# print(mylist)
+
+# The .copy function makes a shallow copy of. A shallow copy mean both lists are stored differently in tyhe memory , but are referncing the same elements
+
+# newlist = mylist.copy()
+# print(newlist)
+
+# sorting is done based on the alphabetical order based on letters 
+
+# mylist = [5, 7, 254 ,  326, 34, 674, 26348579, 34, 67, 89, 7, 69]
+# mylist.sort()
+# mylist.sort(reverse = True)
+# print(mylist)
+
+# The sort function autimatically sorts in ascensing order, but when you write reverse = True it puts them in descending order.
+
+mylist = ["Hello", "Hello", "Bye", "GoodBye"]
+mylist.sort(reverse = True)
+print(mylist)
