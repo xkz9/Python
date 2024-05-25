e8ad0de800f7365c67b88b88fe9c1de305ee8d43
xkz9 <leonardomilanov@gmail.com>
2024-05-25 10:01:18 +0100

    Questions

--- questions.py
+++ questions.py
@@ -2,20 +2,52 @@
 # the highest grade, the lowest grade, and the average grade. Also, classify each grade
 # as 'Fail' (below 60), 'Pass' (60-79), or 'Excellent' (80 and above).
 
-gradeslist = [67, 90, 78, 85, 92, 70, 83] 
+# gradeslist = [67, 90, 78, 85, 92, 70, 83] 
 
-average_grade = sum(gradeslist) / len(gradeslist)
-highest_grade = max(gradeslist) 
-lowest_grade = min(gradeslist)
+# average_grade = sum(gradeslist) / len(gradeslist)
+# highest_grade = max(gradeslist) 
+# lowest_grade = min(gradeslist)
 
-print(highest_grade)
-print(lowest_grade)
-print(average_grade)
-for grade in gradeslist:
+# print(highest_grade)
+# print(lowest_grade)
+# print(average_grade)
+# for grade in gradeslist:
 
-    if grade <= 61: 
-        print("Fail")
-    elif grade >= 61 and grade <= 80:
-        print("Pass")
-    else:
-        print("Excellent")
+#     if grade <= 61: 
+#         print("Fail")
+#     elif grade >= 61 and grade <= 80:
+#         print("Pass")
+#     else:
+#         print("Excellent")
+
+# x = int(input("Enter a number"))
+# y = int(input("Enter a number"))
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("y is greater than x")
+# else:
+#     print("x = y")
+
+# x = int(input("Enter Number"))
+# y = int(input("Enter Number"))
+# z = int(input("Enter Number"))
+
+# if x > y < z:
+#     print("z is the largest")
+# elif x < y < z:
+#     print("z is the largest")
+# elif x > y > z:
+#     print("x is the largest")
+
+# x = "hello, d, g, h"
+# y = "f, d, h, j, k, l, z, x, c ,v ,b ,n ,m"
+# len(x)
+# len(y)
+# if len(x) > len(y):
+#     print("x is greater than y")
+# else:
+#     print("y is greater than x")
+
+# Q. you need to take two strings and check if their first character matches or not. Print yes or no accordingly
